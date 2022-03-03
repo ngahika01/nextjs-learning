951e36e5573f894873ea2b4e42aaafb96436b02f
ngahika01 <mykenjoroge01@gmail.com>
2022-03-03 19:01:16 +0300

    Added layout component

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import Head from "next/head";
+import React from "react";
 
 const about = () => {
   return (
     <div>
-
-        <h1
-        >About</h1>
+      <Head>
+        <title>About this page</title>
+        <meta name="description" content="This is the about page" />
+      </Head>
+      <h1>About</h1>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
