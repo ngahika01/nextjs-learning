71fb39aee426092cbfaee3ad3f3c83d312740aa9
ngahika01 <mykenjoroge01@gmail.com>
2022-03-03 19:57:43 +0300

    getting route names from pages

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+const article = () => {
+    // const router = useRouter();
+    // const { id } = router.query;
+  return <div>this is {id} </div>;
+};
+
+export default article;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
-   <div>
-     <h1>Welcome to next</h1>
-   </div>
-  )
+    <div>
+      <ArticleList articles={articles} />
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=6"
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
