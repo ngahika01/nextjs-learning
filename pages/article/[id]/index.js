9a2acbbb9dee22a1d0561a4be6c1b3dc18203529
ngahika01 <mykenjoroge01@gmail.com>
2022-03-03 20:06:32 +0300

    get serverside props

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,8 +1,26 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
-const article = () => {
-    // const router = useRouter();
-    // const { id } = router.query;
-  return <div>this is {id} </div>;
+const article = ({ article }) => {
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href={"/"}>Go back</Link>
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default article;
